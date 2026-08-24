d4559d536ea850368eba902f1afd035e186731ba
Abubaker1072 <abubakerkhadim79@gmail.com>
2026-08-24 21:13:40 +0500

    Add MySQL seed_data.sql dump and update DatabaseSeeder for MySQL support

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,16 +3,21 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        Schema::disableForeignKeyConstraints();
+
         $this->call([
             CourseSeeder::class,
             PublicationSeeder::class,
             ServiceSeeder::class,
             TestimonialSeeder::class,
         ]);
+
+        Schema::enableForeignKeyConstraints();
     }
 }
